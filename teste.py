66bb39ffa57342d95c222638298b24e3e420aa32
Kelven Cardoso <kelven.pld@gmail.com>
2022-03-21 00:51:41 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkinter import filedialog
+##Janela
+root=Tk("Text Editor")
+root.title("Editor de Texto")
+text=Text(root)
+text.grid()
+text.grid()
+def saveas():
+    global text
+    t = text.get("1.0", "end-1c")
+    savelocation=tkFileDialog.asksaveasfilename()
+    file1=open(savelocation, "w+")
+    file1.write(t)
+    file1.close()
+button=Button(root, text="Save", command=saveas)
+button.grid() 
+root.mainloop()
+
